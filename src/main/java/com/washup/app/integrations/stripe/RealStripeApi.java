fad67f888de763a5fb8bb21e608f6d88be687e5a
Ali Hussain <alihussaincs@gmail.com>
2018-02-19 16:01:23 -0500

    Fix stale stripe customer

--- src/main/java/com/washup/app/integrations/stripe/RealStripeApi.java
+++ src/main/java/com/washup/app/integrations/stripe/RealStripeApi.java
@@ -36,8 +36,7 @@ public class RealStripeApi implements StripeApi {
     Customer customer = Customer.retrieve(stripeCustomerToken);
     Map<String, Object> customerParams = new HashMap<>();
     customerParams.put("source", stripeCardToken);
-    customer.update(customerParams);
-    return customer;
+    return customer.update(customerParams);
   }
 
   @Override
