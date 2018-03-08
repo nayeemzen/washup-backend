66da4fa3919ab1858e472f48899a4cc3ddc2354c
Nayeem Zen <nayeem.zen@mail.utoronto.ca>
2018-03-08 07:47:18 -0500

    Restrict GlobalExceptionHandler to only production

--- src/main/java/com/washup/app/exception/GlobalExceptionHandler.java
+++ src/main/java/com/washup/app/exception/GlobalExceptionHandler.java
@@ -2,12 +2,14 @@ package com.washup.app.exception;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
+@Profile("production")
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
